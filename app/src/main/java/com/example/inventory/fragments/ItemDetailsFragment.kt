485e689ffb1dc51a3c46572a3283042cf6922fce
YeimerrrM <yeimer.mendez@correounivalle.edu.co>
2025-12-03 19:46:31 -0500

    fix ui background

--- app/src/main/java/com/example/inventory/fragments/ItemDetailsFragment.kt
+++ app/src/main/java/com/example/inventory/fragments/ItemDetailsFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.inventory.R
@@ -29,6 +31,13 @@ class ItemDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val mainLayout = view.findViewById<View>(R.id.detail_layout)
+        ViewCompat.setOnApplyWindowInsetsListener(mainLayout) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         val inventoryItem = arguments?.getSerializable("inventory_item") as? Inventory
 
